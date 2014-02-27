86806047b757b3feabda12a9078c21502a491be0
Raphael Kolm <raphael.kolm@gmail.com>
2014-02-27 20:03:32 +0100

    Aperçu des pages du PDF + JARs de IcePDF

--- src/fr/pagelib/termapp/JobSettingsController.java
+++ src/fr/pagelib/termapp/JobSettingsController.java
@@ -1,12 +1,28 @@
 package fr.pagelib.termapp;
 
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.embed.swing.SwingFXUtils;
+
+import org.icepdf.core.exceptions.PDFException;
+import org.icepdf.core.exceptions.PDFSecurityException;
+import org.icepdf.core.pobjects.Document;
+import org.icepdf.core.pobjects.Page;
+import org.icepdf.core.util.GraphicsRenderingHints;
+
+import java.awt.image.BufferedImage;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  *
@@ -21,14 +37,26 @@ public class JobSettingsController extends PageController {
     @FXML Label priceLabel;
     @FXML Button okButton;
 
+    @FXML ImageView pdfImageView;
+    @FXML Label currentPageLabel;
+    @FXML Button previousPageButton;
+    @FXML Button nextPageButton;
+
     SimpleStringProperty pages;
     SimpleBooleanProperty color;
     SimpleStringProperty copies;
+
+    Document pdfDocument;
+    Integer totalPages;
+    SimpleIntegerProperty currentPage;
 
     public JobSettingsController() {
         pages = new SimpleStringProperty();
         color = new SimpleBooleanProperty();
         copies = new SimpleStringProperty();
+
+        currentPage = new SimpleIntegerProperty(-1);
+        totalPages = 0;
     }
 
     public void initialize() {
@@ -44,12 +72,69 @@ public class JobSettingsController extends PageController {
                 greyLevelsHintLabel.setVisible(!newValue);
             }
         });
+
+        currentPage.addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> obs, Number oldValue, Number newValue) {
+                Integer p = (Integer) newValue;
+
+                // Navigation stuff
+                currentPageLabel.setText(String.format("%d / %d", p + 1, totalPages));
+                previousPageButton.setDisable(p == 0);
+                nextPageButton.setDisable(p == totalPages - 1);
+
+                // Show selected page
+                BufferedImage awtImage = (BufferedImage) pdfDocument.getPageImage(
+                        p, GraphicsRenderingHints.PRINT, Page.BOUNDARY_CROPBOX, 0.0f, 1.0f);
+                Image fxImage = SwingFXUtils.toFXImage(awtImage, null);
+                pdfImageView.setImage(fxImage);
+            }
+        });
     }
 
     public void reset() {
         pages.set("");
         color.set(false);
         copies.set("1");
+
+        if (pdfDocument != null) pdfDocument.dispose();
+
+        // Load PDF document
+        try {
+            pdfDocument = new Document();
+            pdfDocument.setFile(mainController.getCurrentDocumentPath());
+
+            totalPages = pdfDocument.getNumberOfPages();
+            currentPage.set(0);
+
+            System.out.println(String.format("PDF document loaded, %d pages", totalPages));
+        }
+        catch (PDFException e) {
+            System.out.println("Error parsing PDF document: " + e);
+            pdfDocument = null;
+        }
+        catch (PDFSecurityException e) {
+            System.out.println("Security error parsing PDF document: " + e);
+            pdfDocument = null;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("PDF document not found");
+            pdfDocument = null;
+        }
+        catch (IOException e) {
+            System.out.println("IOException opening PDF document: " + e);
+            pdfDocument = null;
+        }
     }
 
+    @FXML
+    public void previousPage() {
+        int p = currentPage.get();
+        if (p > 0) currentPage.set(p - 1);
+    }
+
+    @FXML
+    public void nextPage() {
+        int p = currentPage.get();
+        if (p < totalPages - 1) currentPage.set(p + 1);
+    }
 }
